50c1392fee993d86b76df425fc02fc2dd8905d69
Brendan Patrick Clarke <clarkeb3@tcd.ie>
2018-02-12 14:07:19 +0000

    Tasks 1 and 2 now completed and commented

---
+++ my_seq.c
@@ -0,0 +1,60 @@
+/* Including libraries needed to run the code. */
+
+#include <stdio.h>	
+#include <math.h>	
+#include <stdlib.h> 
+#include <getopt.h>
+
+/* Setting up the main function of the program */
+
+int main(int argc, char *argv[]) /* Using required syntax in main() arguments in order to use getopt() */
+{ 
+	/* Defining/initialising the key variables of the program */
+
+	int FIRST = 1;
+	int INCREMENT = 1;
+	int LAST;
+	int padding = 0;
+	char str[20];
+	int i = 0;
+
+	int option = getopt(argc, argv, "w"); /* Setting up -w option */
+
+	/* This sets i = 1 when the option -w is used. */
+	if(option != -1) {
+		i = 1;}
+
+	/* Prints error when too few options are inputted. */
+	if(argc < 2 + i) {
+		printf("You must use an addtional option.\n");		
+		return 0;}
+
+	/* Prints error when too many options are inputted. */
+	if(argc > 4 + i) {
+		printf("You have used too many options.\n");		
+		return 0;}
+
+	/* This sets the LAST variable for the case in which the user only inputs one number (e.g ./my_seq 25).*/
+	if(argc == 2 + i) {
+		LAST = atoi(argv[1]);}
+
+	/* This sets the FIRST and LAST variables for the case in which the user inputs two number (e.g ./my_seq 25 50).*/
+	if(argc == 3 + i) {
+		FIRST = atoi(argv[1]);
+		LAST = atoi(argv[2]);}
+
+	/* This sets the FIRST, INCREMENT and LAST variables for the case in which the user inputs three number (e.g ./my_seq 25 5 50).*/
+	if(argc == 4 + i) {
+		FIRST = atoi(argv[1]);
+		INCREMENT = atoi(argv[2]);
+		LAST = atoi(argv[3]);}
+
+	/* This while loop prints out the sequence of numbers. It adds leading zeroes when -w is selected (when i == 1). */ 
+	while(FIRST <= LAST) {
+		if(i == 1) {
+			padding = (floor(log10(abs(LAST)))) + 1;}
+		sprintf(str, "%%0%dd \n", padding);
+		printf(str, FIRST);
+		FIRST += INCREMENT;
+	}
+}
--- round.c
+++ round.c
@@ -1,49 +1,71 @@
-#include <stdio.h>	// Including library required to run the code.
-#include <math.h>	// Including library required to run the code.
-#include <stdlib.h> // for atoi, atof
+/* Including libraries needed to run the code. */
+
+#include <stdio.h>	
+#include <math.h>	
+#include <stdlib.h> 
 #include <getopt.h>
 
+/* Setting up the function of the program that prints out instructions for usage. This prints when the user incorrectly runs the program.*/
 void print_usage() {
 	printf("Usage: ./round.o [Number to be rounded] [-option(for unit)] <rounded number in preffered units> \n");
 	exit(1);
 }
+
+/* Setting up the function for -K option. */
 void input_number_K(float input_number) {
 	input_number = input_number/1024.;
 	printf("%.1fK \n", input_number);
 }
+
+/* Setting up the function for -M option. */
 void input_number_M(float input_number) {
 	input_number = input_number/1048576.;
 	printf("%.1fM \n", input_number);
 }
+
+/* Setting up the function for -G option. */
 void input_number_G(float input_number) {
 	input_number = input_number/1073741824.;
 	printf("%.1fG \n", input_number);
 }
+
+/* Setting up the function for -T option. */
 void input_number_T(float input_number) {
 	input_number = input_number/1099511627776.;
 	printf("%.1fT \n", input_number);
 }
+
+/* Setting up the function for -P option. */
 void input_number_P(float input_number) {
 	input_number = input_number/1125899906842624.;
 	printf("%.1fP \n", input_number);
 }
+
+/* Setting up the function for -E option. */
 void input_number_E(float input_number) {
 	input_number = input_number/1152921504606847000.;
 	printf("%.1fE \n", input_number);
 }
-int main(int argc, char **argv) 
+
+/* Setting up the main function of the program */
+int main(int argc, char **argv) /* Using required syntax in main() arguments in order to use getopt() */
 {
-	if (argc < 2) {
+	if (argc < 2) {					/* Prints error when too few options are inputted. */	
 			print_usage();
 	}
 	if (argc > 3) {
-			print_usage();
+			print_usage();			/* Prints error when too many options are inputted. */	
 	}	
+
+	/* Defining/initialising the key variables/strings of the program. */
 	float the_passed_number;
 	int option;
 	int unit = 1024;
 	int index = 0;
 	const char suffixes[] = {' ', 'K', 'M', 'G', 'T', 'P', 'E'};
+
+	/* This loops when a valid option is selected or when the argument count is 2. */
+	/* Depending on the chosen case, the program will round to a particular unit. */
 	while((option = getopt(argc, argv, "K:M:G:T:P:E:h:")) != -1 || argc == 2)
 	{ 
 		switch(option) 
@@ -69,6 +91,7 @@ int main(int argc, char **argv)
 				default: 
 					print_usage();
 					break;
+				/* Human readable case. */
 				case 'h' :
 					the_passed_number = (atof(optarg));
 					while (the_passed_number >= unit) {
